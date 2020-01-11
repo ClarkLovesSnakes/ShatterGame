b3895790ef118956578b13bd0892b0ac592ca2cf
SpikeClub-josh <59776573+SpikeClub-josh@users.noreply.github.com>
2020-01-11 15:36:36 -0600

    Update Word.py

--- Word.py
+++ Word.py
@@ -1,18 +1,23 @@
 
 from random import randint
 
+
 class Word :
 
+    # initialize object
+    # set x and y positions, set word, set speed
     def __init__(self, wordString, speed):
 
         self.wordString = wordString
         self.x = randint(0, 1000)
         self.y = 0
         self.dy = speed
+        self.active = True
 
-
+    # should be called every frame before render
+    # updates y position, if off sreen, sets active to "false"
     def update(self):
+
         self.y += self.dy
         if self.y > 600 :
-            destroy()
-
+            self.active = False
