27fe7c2967151c30c36e2014375e040f014f3339
ElijahBenMagee <48970303+ElijahBenMagee@users.noreply.github.com>
2020-01-11 14:36:03 -0600

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,14 +1,19 @@
 import pygame
 #import pygame.sprite.Sprite as Sprite
 
+class Game:
+    def __init__(self):
+        pass
+
+
+
+
+
 def main():
 
     pygame.init()
-
     pygame.display.set_caption("test")
-
     screen = pygame.display.set_mode((1920,1080))
-
     running = True
 
     while running:
