a101cc1292000df6dc2cc878495ded60d6e4600a
SpikeClub-josh <59776573+SpikeClub-josh@users.noreply.github.com>
2020-01-11 14:28:26 -0600

    Basic Structure

--- Testing.py
+++ Testing.py
@@ -1,29 +1,33 @@
 import pygame
-#import pygame.sprite.Sprite as Sprite
 
-def main():
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+is_blue = True
+x = 30
+y = 30
 
-    pygame.init()
+clock = pygame.time.Clock()
 
-    pygame.display.set_caption("test")
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_blue = not is_blue
 
-    screen = pygame.display.set_mode((1920,1080))
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]: y -= 3
+    if pressed[pygame.K_DOWN]: y += 3
+    if pressed[pygame.K_LEFT]: x -= 3
+    if pressed[pygame.K_RIGHT]: x += 3
 
-    # COMMENT by josh
-    running = True
+    screen.fill((0, 0, 0))
+    if is_blue:
+        color = (0, 128, 255)
+    else:
+        color = (255, 100, 0)
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
 
-    while running:
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-
-                running = False
-
-main()
-
-
-for i in range(10):
-    print(i)
-
-print("Elijah's Test")
+    pygame.display.flip()
+    clock.tick(60)
