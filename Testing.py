2c5666548a40cf40a0a530571b0f9cb79b85646b
ClarkLovesSnakes <44585059+ClarkLovesSnakes@users.noreply.github.com>
2020-01-11 13:53:28 -0600

    Create Testing.py

---
+++ Testing.py
@@ -0,0 +1,26 @@
+import pygame
+#import pygame.sprite.Sprite as Sprite
+
+def main():
+
+    pygame.init()
+
+    pygame.display.set_caption("test")
+
+    screen = pygame.display.set_mode((1920,1080))
+
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+
+                running = False
+
+main()
+
+
+for i in range(10):
+    print(i)
