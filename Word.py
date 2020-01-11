8d5f58f54e2bc53ab053c3251a740412fc1d6bce
SpikeClub-josh <59776573+SpikeClub-josh@users.noreply.github.com>
2020-01-11 16:15:24 -0600

    Word updates

--- Word.py
+++ Word.py
@@ -6,13 +6,26 @@ class Word :
 
     # initialize object
     # set x and y positions, set word, set speed
-    def __init__(self, wordString, speed):
+    def __init__(self, originalWord, speed, swaps, deletions):
 
-        self.wordString = wordString
+        # inintialize variables
+        self.originalWord = originalWord
+        self.scrambledWord = originalWord
         self.x = randint(0, 1000)
         self.y = 0
         self.dy = speed
         self.active = True
+
+        length = len(originalWord)
+
+        # execute swaps
+        for i in range(swaps) :
+            swapPos1 = randint(0, length - 1)
+            swapPos2 = randint(0, length - 1)
+            temp = self.scrambledWord[swapPos1]
+            self.scrambledWord[swapPos1] = self.scrambledWord[swapPos2]
+            self.scrambledWord[swapPos2] = temp
+
 
     # should be called every frame before render
     # updates y position, if off sreen, sets active to "false"
