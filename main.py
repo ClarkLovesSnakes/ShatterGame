f37285fae59009e48eaae6b0ffc1fe16b62e7352
ElijahBenMagee <48970303+ElijahBenMagee@users.noreply.github.com>
2020-01-11 19:38:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -33,9 +33,13 @@ text2 = [pygame.font.Font('freesansbold.ttf', 50).render('START', True, white, c
 text3 = [pygame.font.Font('freesansbold.ttf', 50).render('LEVEL', True, white, color), [426,185]]
 text4 = [pygame.font.Font('freesansbold.ttf', 50).render('ABOUT', True, white, color), [100,421]]
 text5 = [pygame.font.Font('freesansbold.ttf', 50).render('QUIT', True, white, color), [446,421]]
-text6 = [pygame.font.Font('freesansbold.ttf', 50).render('QUIT', True, white, color), [446,421]]
-
-
+text6 = [pygame.font.Font('freesansbold.ttf', 35).render('HIGHSCORES', True, white, black), [700,130]]
+text7 = [pygame.font.Font('freesansbold.ttf', 20).render('Elijah            5280', True, white, black), [700,200]]
+text8 = [pygame.font.Font('freesansbold.ttf', 20).render('Elijah            5223', True, white, black), [700,230]]
+text9 = [pygame.font.Font('freesansbold.ttf', 20).render('Elijah            5145', True, white, black), [700,260]]
+text10 = [pygame.font.Font('freesansbold.ttf', 20).render('Elijah            4980', True, white, black), [700,290]]
+text11 = [pygame.font.Font('freesansbold.ttf', 20).render('Josh              23', True, white, black), [700,320]]
+texts = [text2,text3,text4,text5,text6,text7,text8,text9,text10,text11]
 
 
 
@@ -65,12 +69,13 @@ while not done:
     pygame.draw.rect(screen, color, pygame.Rect(button3[0], button3[1], 250, 150))
     pygame.draw.rect(screen, color, pygame.Rect(button4[0], button4[1], 250, 150))
 
-    if gameTime > 30:
+    if gameTime < 30:
         screen.blit(text, textRect)
-    screen.blit(text2[0], text2[1])
-    screen.blit(text3[0], text3[1])
-    screen.blit(text4[0], text4[1])
-    screen.blit(text5[0], text5[1])
+    for i in range(len(texts)):
+            screen.blit(texts[i][0], texts[i][1])
+
+
+
 
 
     pygame.display.flip()
